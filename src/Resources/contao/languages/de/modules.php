1c8fee524848f453a20df5ea3bfcf19846f463d1
jonnysp <jonnyspitzner@googlemail.com>
2018-01-05 14:29:56 +0100

    add CTE description

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -4,3 +4,7 @@ $GLOBALS['TL_LANG']['MOD']['architekt'] = 'Architekt';
 $GLOBALS['TL_LANG']['MOD']['referenzen'] = array('Referenzen','Verwalte die Referenzen');
 $GLOBALS['TL_LANG']['MOD']['hausplaner'] = array('Hausplaner','Verwalte die Hauspläne');
 $GLOBALS['TL_LANG']['MOD']['partner'] = array('Partner','Verwalte die Partner');
+
+$GLOBALS['TL_LANG']['CTE']['referenzen_archiv'] = array("Referenzen-Archiv","");
+$GLOBALS['TL_LANG']['CTE']['partner_archiv'] = array("Partner-Archiv","");
+$GLOBALS['TL_LANG']['CTE']['hausplaner_viewer'] = array("Hausplaner","");
